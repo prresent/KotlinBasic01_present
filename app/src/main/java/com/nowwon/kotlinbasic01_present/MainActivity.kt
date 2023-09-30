3fce788ad685bc2b55bd5b8f5966276fb793cba5
present <present@sookmyung.ac.kr>
2023-09-30 20:27:04 +0900

    로그 찍어보기

--- app/src/main/java/com/nowwon/kotlinbasic01_present/MainActivity.kt
+++ app/src/main/java/com/nowwon/kotlinbasic01_present/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nowwon.kotlinbasic01_present
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.clickBtn
 import kotlinx.android.synthetic.main.activity_main.smallBtn
 
@@ -10,7 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        clickBtn
+        clickBtn.setOnClickListener {
+
+            // 코드에 메모 남기기 - 주석
+//            ctrl + /: 커서가 있는 줄을 주석처리.
+//            이 { } 안에 있는 코드만, 버튼이 눌렸을 때 실행됨.
+
+            Log.d("메인화면로그", "클릭용 버튼이 눌림")
+            Log.e("메인화면로그", "에러 관련 로그")
+
+        }
         smallBtn
 
     }
