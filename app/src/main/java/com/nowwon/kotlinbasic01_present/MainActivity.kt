64f7acb08d70999dd3e6cf0b437e0e16f41c84de
present <present@sookmyung.ac.kr>
2023-09-30 20:13:57 +0900

    버튼 추가 -> 기초 이벤트에 사용할 예정

--- app/src/main/java/com/nowwon/kotlinbasic01_present/MainActivity.kt
+++ app/src/main/java/com/nowwon/kotlinbasic01_present/MainActivity.kt
@@ -2,10 +2,16 @@ package com.nowwon.kotlinbasic01_present
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.clickBtn
+import kotlinx.android.synthetic.main.activity_main.smallBtn
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickBtn
+        smallBtn
+
     }
 }
